c0d85fd9ab12f4ae3d58c18611060edb53104c70
AdamJHowell <adam.j.howell@gmail.com>
2020-07-25 19:52:02 -0600

    Update MQTTTest.ino
    3 - Reconfigured for 127.0.0.1
    
    Not working with Edge yet.

--- MQTTTest/MQTTTest.ino
+++ MQTTTest/MQTTTest.ino
@@ -1,12 +1,16 @@
 #include <ESP8266WiFi.h>                  // Include the WiFi library.
 #include <MQTT.h>                         // Include the MQTT library.
 #include <Wire.h>                         // Include Wire library, required for I2C devices
+#include <Arduino_JSON.h>                 // Include the JSON library.
 
 const char* ssid     = "Red";             // The case-sensitive SSID of the WiFi network you want to connect to.
 const char* password = "8012254722";      // The password of the WiFi network.
+const char* broker = "127.0.0.1";         // The MQTT broker name.
+const String mqttTopic = "MQTTTest001";    // The MQTT topic.
 WiFiClient wifiClient;
-MQTTClient mqttClient;
+MQTTClient mqttClient( 512 );             // Create a client with a 512 byte buffer.  The default is 128 bytes.
 unsigned long lastMillis = 0;
+unsigned long loopCount = 0;
 
 
 void connect()
@@ -19,7 +23,7 @@ void connect()
   }
 
   Serial.print( "\nMQTT connecting..." );
-  while ( !mqttClient.connect( "arduino", "try", "try" ) )
+  while ( !mqttClient.connect( "arduino" ) )
   {
     Serial.print( "." );
     delay( 1000 );
@@ -27,9 +31,9 @@ void connect()
 
   Serial.println( "\nMQTT connected!" );
 
-  mqttClient.subscribe( "/hello" );
+//  mqttClient.subscribe( "/hello" );
   // mqttClient.unsubscribe( "/hello" );
-}
+} // End of connect() function.
 
 
 void messageReceived( String &topic, String &payload )
@@ -40,7 +44,43 @@ void messageReceived( String &topic, String &payload )
   // unsubscribe as it may cause deadlocks when other things arrive while
   // sending and receiving acknowledgments. Instead, change a global variable,
   // or push to a queue and handle it in the loop after calling `client.loop()`.
-}
+} // End of messageReceived() function.
+
+
+/**
+ * Build a JSON object for the CreatePersistenceTopic operation.
+ * This operation will create a MQTT topic that can be published to.
+ * This operation will also create a table in cTree Edge.
+ * Consecutive calls to this function will not alter the topic or table.
+ */
+void createTopic( String topic )
+{
+  JSONVar createObject;
+  createObject["operation"] = "CreatePersistenceTopic";
+  createObject["persistenceTopic"] = topic;
+  createObject["tableName"] = topic;          // It is customary, but not required, for the table name to match the topic.
+  createObject["mapOfPropertiesToFields"]["propertyPath"] = "BIGINT001";
+  createObject["mapOfPropertiesToFields"]["fieldName"] = "BIGINT001";
+  createObject["mapOfPropertiesToFields"]["fieldType"] = "BIGINT";
+  
+  mqttClient.publish( "ctreeAdministration", createObject );
+} // End of createTopic() function.
+
+
+/**
+ * Build a JSON object for the DeletePersistenceTopic operation.
+ * This operation will delete an existing MQTT topic.
+ * This operation does not delete the table in cTree Edge.
+ * Consecutive calls to this function will have no effect aside from logging an error in Edge.
+ */
+void deleteTopic( String topic )
+{
+  JSONVar deleteObject;
+  deleteObject["operation"] = "DeletePersistenceTopic";
+  deleteObject["persistenceTopic"] = topic;
+  
+  mqttClient.publish( "ctreeAdministration", deleteObject );
+} // End of deleteTopic() function.
 
 
 void setup()
@@ -79,11 +119,13 @@ void setup()
 
   pinMode( LED_BUILTIN, OUTPUT );      // Initialize digital pin LED_BUILTIN as an output.
 
-  mqttClient.begin( "broker.shiftr.io", wifiClient );
+  mqttClient.begin( "127.0.0.1", wifiClient );
   mqttClient.onMessage( messageReceived );
 
   connect();
-}
+
+   createTopic( mqttTopic );
+} // End of setup() function.
 
 
 void loop()
@@ -99,13 +141,20 @@ void loop()
   if ( millis() - lastMillis > 1000 )
   {
     lastMillis = millis();
-    mqttClient.publish( "/hello", "world" );
+    // bool publish(const String &topic, const String &payload);
+//    mqttClient.publish( "/hello", "world" );
+    char * myStr;
+    sprintf( myStr, "%lu", loopCount );
+    mqttClient.publish( mqttTopic, myStr );
   }
 
   digitalWrite( LED_BUILTIN, HIGH );   // Turn the LED on.
   delay( 1000 );                       // Wait for one second.
   digitalWrite( LED_BUILTIN, LOW );    // Turn the LED off.
   delay( 1000 );                       // Wait for one second.
+  loopCount++;
+  Serial.print( "Loop count: " );
+  Serial.println( loopCount );
   Serial.print( "IP address:\t" );
   Serial.println( WiFi.localIP() );    // Send the IP address of the ESP8266 to the serial port.
-}
+} // End of loop() function.
